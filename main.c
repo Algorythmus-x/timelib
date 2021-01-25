117ce629985b819750b53f189ccd6ee4f796d5f1
Armin Spöllmann <aspoellm@hellmann.com>
2021-01-25 10:46:32 +0100

    added is_leapyear function

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /**
- *
- *
- *
+ * Tag des Jahres
+ * Autor: Armin
+ * Datum: 25.01.2021
  **/
 
 #include <stdio.h>
@@ -11,14 +11,34 @@
  *
  *
  **/
-int day_of_the_year(int day, int month, int year)
-{
+int day_of_the_year(int day, int month, int year){
 
     return 0;
 }
 
-int main()
-{
+int is_leapyear(int year){
+    if(year < 1582){
+        return -1;
+    }
+    if(year % 4 == 0){  //Prüft ob jahr ohne rest durch 4 teilbar ist
+        if(year % 100 == 0){    //Prüft ob jahr ohne rest durch 100 teilbar ist
+            if(year % 400 == 0){    //Prüft ob jahr ohne rest durch 400 teilbar ist
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            return 1;
+        }
+    }
+    else{
+        return 0;
+    }
+}
+
+int main(){
     printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
     return 0;
 }
