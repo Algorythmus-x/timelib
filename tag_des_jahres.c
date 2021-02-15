c9005c7f2ea8aa1ba6067d598813b4c0c4bcfb43
Armin <>
2021-02-15 11:27:34 +0100

    functions moved to an extra file

---
+++ tag_des_jahres.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tag_des_jahres.h"
+
+int is_leapyear(int year){
+    if(year < 1582 || year > 2400){
+        return -1;
+    }
+    if(year % 4 == 0){  //Prüft ob jahr ohne rest durch 4 teilbar ist
+        if(year % 100 == 0){    //Prüft ob jahr ohne rest durch 100 teilbar ist
+            if(year % 400 == 0){    //Prüft ob jahr ohne rest durch 400 teilbar ist
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            return 1;
+        }
+    }
+    else{
+        return 0;
+    }
+}
+
+int get_days_for_month(int month, int year){
+    int daysPerMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    if(is_leapyear(year) == 1){
+        daysPerMonth[1] = 29;
+    }
+    return daysPerMonth[(month) - 1];
+}
+
+int exists_date(int day, int month, int year){
+    if(is_leapyear(year) == -1){
+        printf("Ungueltiges Jahr\n\n");
+        return 0;
+    }
+    if(month <= 0 || month > 12){
+        printf("Ungueltiger Monat\n\n");
+        return 0;
+    }
+    else {
+        //Überprüfung ob Tag gültig ist
+        if(day <= 0 || day > get_days_for_month(month, year)){
+            printf("Ungueltiger Tag\n\n");
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+}
+
+void input_date(int *year, int *month, int *day){
+    do{
+        *day = 0;
+        *month = 0;
+        *year = 0;
+        printf("Bitte geben Sie das Jahr an: ");
+        scanf("%i", year);
+        fflush(stdin);
+
+        printf("Bitte geben Sie den Monat ein: ");
+        scanf("%i", month);
+        fflush(stdin);
+
+        printf("Bitte geben Sie den Tag ein: ");
+        scanf("%i", day);
+        fflush(stdin);
+    }
+    while(exists_date(*day, *month, *year) != 1);
+
+}
+
+int day_of_the_year(int day, int month, int year){
+    int sumDays = 0;
+    for(int i = 1; i < month;i++){
+        sumDays = sumDays + get_days_for_month(i, year);
+    }
+    sumDays = sumDays + day;
+    return sumDays;
+}
---
+++ tag_des_jahres.h
@@ -0,0 +1,10 @@
+#ifndef TAG_DES_JAHRES_H_INCLUDED
+#define TAG_DES_JAHRES_H_INCLUDED
+
+int day_of_the_year(int day, int month, int year);
+int get_days_for_month(int month, int year);
+int exists_date(int day, int month, int year);
+void input_date();
+int is_leapyear(int year);
+
+#endif // TAG_DES_JAHRES_H_INCLUDED
